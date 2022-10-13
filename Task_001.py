ab1fc51b7ad2ba1aea149a57bdc58d2b9f84bf4a
GeorgeSaltykov <sonofpeley@gmail.com>
2022-10-13 19:49:01 +0300

    Merge and add third task

--- Task_001.py
+++ Task_001.py
@@ -17,40 +17,66 @@
 
 # Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 
-x = [0, 0, 0, 1, 0, 1, 1, 1]
-y = [0, 0, 1, 0, 1, 1, 0, 1]
-z = [0, 1, 0, 0, 1, 0, 1, 1]
+# x = [0, 0, 0, 1, 0, 1, 1, 1]
+# y = [0, 0, 1, 0, 1, 1, 0, 1]
+# z = [0, 1, 0, 0, 1, 0, 1, 1]
 
-l = len(x)
+# l = len(x)
 
-def disjunction(a, b):
-    c = []
-    for i in range(l):
-        if a[i] == 1 or b[i] == 1:
-            c.append(1)
-        else:
-            c.append(0)
-    return c
+# def disjunction(a, b):
+#     c = []
+#     for i in range(l):
+#         if a[i] == 1 or b[i] == 1:
+#             c.append(1)
+#         else:
+#             c.append(0)
+#     return c
 
-def inversion(a):
-    c = []
-    for i in range(l):
-        if a[i] == 1:
-            c.append(0)
-        elif a[i] == 0:
-            c.append(1)
-    return c
+# def inversion(a):
+#     c = []
+#     for i in range(l):
+#         if a[i] == 1:
+#             c.append(0)
+#         elif a[i] == 0:
+#             c.append(1)
+#     return c
 
-def conjunction(a, b):
-    c = []
-    for i in range(l):
-        if a[i] == 1 and b[i] == 1:
-            c.append(1)
-        else:
-            c.append(0)
-    return c
+# def conjunction(a, b):
+#     c = []
+#     for i in range(l):
+#         if a[i] == 1 and b[i] == 1:
+#             c.append(1)
+#         else:
+#             c.append(0)
+#     return c
 
-if inversion(disjunction(disjunction(x, y), z)) == conjunction(conjunction(inversion(x), inversion(y)), inversion(z)):
-    print('Yes')
-else:
-    print('No')
+# if inversion(disjunction(disjunction(x, y), z)) == conjunction(conjunction(inversion(x), inversion(y)), inversion(z)):
+#     print('Yes')
+# else:
+#     print('No')
+
+# Напишите программу, которая принимает на вход координаты точки (X и Y),
+# причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости,
+# в которой находится эта точка (или на какой оси она находится).
+# Пример:
+# - x=34; y=-30 -> 4
+# - x=2; y=4-> 1
+# - x=-34; y=-30 -> 3
+
+# x = int(input('Введите х не равный нулю: '))
+# y = int(input('Введите y не равный нулю: '))
+
+# if x > 0 and y > 0:
+#     print('Точка находится в 1-ой четверти плоскости')
+# elif x < 0 and y > 0:
+#     print('Точка находится во 2-ой четверти плоскости')
+# elif x < 0 and y < 0:
+#     print('Точка находится в 3-ей четверти плоскости')
+# elif x > 0 and y < 0:
+#     print('Точка находится в 4-ой четверти плоскости')
+# else:
+#     print('Что-то пошло не так, попробуйте снова')
+
+# Напишите программу, которая по заданному номеру четверти, 
+# показывает диапазон возможных координат точек в этой четверти (x и y).
+
