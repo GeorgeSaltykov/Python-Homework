3de0f2d95fab7d9db6a1cd2367b2e958e5ba834a
GeorgeSaltykov <sonofpeley@gmail.com>
2022-10-29 04:00:02 +0600

    End

--- Sem_005.py
+++ Sem_005.py
@@ -1,9 +1,10 @@
 # Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
+import setuptools.errors
 
-# user_text = input('Введите текст: ').split()
-# words = 'абв'
-# res = [i for i in user_text if words not in i]
-# print(res)
+user_text = input('Введите текст: ').split()
+words = 'абв'
+res = [i for i in user_text if words not in i]
+print(res)
 
 # Создайте программу для игры с конфетами человек против человека.
 # Условие задачи: На столе лежит 2021 конфета. Играют два игрока делая ход друг после друга.
@@ -11,45 +12,45 @@
 # Все конфеты оппонента достаются сделавшему последний ход.
 # Сколько конфет нужно взять первому игроку, чтобы забрать все конфеты у своего конкурента?
 
-# print('Давайте сыграем в игру!\n На столе лежит 2021 конфета. Играют два игрока, делая ход друг после друга.\n'
-#       'Первый ход определяется жеребьёвкой. За один ход можно забрать не более чем 28 конфет.\n'
-#       'Все конфеты оппонента достаются сделавшему последний ход.')
-# sweets = 2021
-# count = 0
-# stop = True
-# while sweets > 0:
-#     while sweets > 0:
-#         if stop == False:
-#             stop = True
-#             break
-#         first_user = int(input('Игрок 1. Сколько конфет вы возьмете? '))
-#         if 0 < first_user < 29:
-#             sweets -= first_user
-#             count += 1
-#             print('Конфет осталось: ', sweets)
-#             break
-#         else:
-#             print('Вы ввели неподходящее число конфет')
-#             stop = False
-#             break
-#     while sweets > 0:
-#         if stop == False:
-#             stop = True
-#             break
-#         second_user = int(input('Игрок 2. Сколько конфет вы возьмете? '))
-#         if 0 < second_user < 29:
-#             sweets -= second_user
-#             count += 1
-#             print('Конфет осталось: ', sweets)
-#             break
-#         else:
-#             print('Вы ввели неподходящее число конфет')
-#             stop = False
-#             break
-# if count % 2 == 0:
-#     print('Победил второй игрок!')
-# else:
-#     print('Победил первый игрок')
+print('Давайте сыграем в игру!\n На столе лежит 2021 конфета. Играют два игрока, делая ход друг после друга.\n'
+      'Первый ход определяется жеребьёвкой. За один ход можно забрать не более чем 28 конфет.\n'
+      'Все конфеты оппонента достаются сделавшему последний ход.')
+sweets = 2021
+count = 0
+stop = True
+while sweets > 0:
+    while sweets > 0:
+        if stop == False:
+            stop = True
+            break
+        first_user = int(input('Игрок 1. Сколько конфет вы возьмете? '))
+        if 0 < first_user < 29:
+            sweets -= first_user
+            count += 1
+            print('Конфет осталось: ', sweets)
+            break
+        else:
+            print('Вы ввели неподходящее число конфет')
+            stop = False
+            break
+    while sweets > 0:
+        if stop == False:
+            stop = True
+            break
+        second_user = int(input('Игрок 2. Сколько конфет вы возьмете? '))
+        if 0 < second_user < 29:
+            sweets -= second_user
+            count += 1
+            print('Конфет осталось: ', sweets)
+            break
+        else:
+            print('Вы ввели неподходящее число конфет')
+            stop = False
+            break
+if count % 2 == 0:
+    print('Победил второй игрок!')
+else:
+    print('Победил первый игрок')
 
 # Создайте программу для игры в ""Крестики-нолики"".
 
@@ -59,14 +60,18 @@ print('Крестики-нолики! \n'
 
 dict = {1: '[1]', 2: '[2]', 3: '[3]', 4: '[4]', 5: '[5]', 6: '[6]', 7: '[7]', 8: '[8]', 9: '[9]'}
 field_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-count = 1
-for i in range(3):
-    for j in range(3):
-        print(dict[count], end=' ')
-        count += 1
-    print()
+def showfield(field):
+    count = 1
+    for i in range(3):
+        for j in range(3):
+            print(dict[count], end=' ')
+            count += 1
+        print()
+
+
 end = True
 stop = True
+showfield(dict)
 while end == True:
     while end == True:
         if stop == False:
@@ -76,12 +81,7 @@ while end == True:
         if first_player in field_numbers:
             dict[first_player] = '[X]'
             field_numbers.remove(first_player)
-            count = 1
-            for i in range(3):
-                for j in range(3):
-                    print(dict[count], end=' ')
-                    count += 1
-                print()
+            showfield(dict)
             break
         else:
             print('Вы ввели неподходящее число')
@@ -102,12 +102,7 @@ while end == True:
         if second_player in field_numbers:
             dict[second_player] = '[O]'
             field_numbers.remove(second_player)
-            count = 1
-            for i in range(3):
-                for j in range(3):
-                    print(dict[count], end=' ')
-                    count += 1
-                print()
+            showfield(dict)
             break
         else:
             print('Вы ввели неподходящее число')
@@ -119,3 +114,51 @@ while end == True:
         or (dict[2] == '[O]' and dict[5] == '[O]' and dict[8] == '[O]') or (dict[3] == '[O]' and dict[6] == '[O]' and dict[9] == '[O]')):
         print('Игрок 2 победил')
         end = False
+
+# Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
+# Входные и выходные данные хранятся в отдельных текстовых файлах.
+
+in_path = 'input.txt'
+out_path = 'output.txt'
+
+
+def compression(file):
+    dictionary = []
+    for i in file:
+        if i not in dictionary:
+            dictionary.append(i)
+    res = ''
+    for i in dictionary:
+        count = 0
+        for j in file:
+            if i == j:
+                count += 1
+        res += str(count) + i
+    return res
+
+
+def recovery(file = ''):
+    recovery_file = ''
+    for i in range(len(file)):
+        if file[i].isdigit():
+            for j in range(int(file[i])):
+                recovery_file += file[i + 1]
+    return recovery_file
+
+
+def writing(path, compress):
+    with open(path, 'w') as res:
+        res.writelines(compress)
+
+
+def opening(path):
+    data = ''
+    with open(path, 'r') as line:
+        for lines in line:
+            if lines != None:
+                data = lines
+    return data
+
+
+writing(out_path, compression(opening(in_path)))
+print(recovery(opening(out_path)))
